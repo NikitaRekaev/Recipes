ff079d0e01e23dd3b2f54c7db12b1d43b8f63747
NikitaRekaev <nvvvvk@gmail.com>
2022-09-20 23:34:35 +0200

    Create project

---
+++ Recipes/Sources/Supporting/Application/AppDelegate.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
+                     [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let navController = UINavigationController(rootViewController: MainViewController())
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = navController
+        window.makeKeyAndVisible()
+        window.overrideUserInterfaceStyle = .light
+        self.window = window
+        return true
+    }
+}
+
