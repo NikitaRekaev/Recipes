5e4f1108bc6fd9fe0d2e9435636aab96ad149d1b
NikitaRekaev <nvvvvk@gmail.com>
2022-10-27 16:06:34 +0600

    Added CollectionViewCellRepresentable for RecommendedImageCollectionViewCellViewModel

--- Recipes/Sources/Modules/DetailsScreen/ViewModels/RecommendedImageCollectionViewCellViewModel.swift
+++ Recipes/Sources/Modules/DetailsScreen/ViewModels/RecommendedImageCollectionViewCellViewModel.swift
@@ -21,5 +21,23 @@ final class RecommendedImageCollectionViewCellViewModel {
         self.imageLink = imageLink
         self.uuid = uuid
     }
+}
+
+// MARK: - CollectionViewCellRepresentable
+
+extension RecommendedImageCollectionViewCellViewModel: CollectionViewCellRepresentable {
     
+    static func registerCell(collectionView: UICollectionView) {
+        RecommendedCollectionViewCell.registerCell(collectionView: collectionView)
+    }
+    
+    func dequeueCell(collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = RecommendedCollectionViewCell.dequeueCell(collectionView: collectionView, indexPath: indexPath)
+        cell.setupCellData(viewModel: self)
+        return cell
+    }
+    
+    func cellSelected() {
+        self.didSelectRecommendedRecipe?(uuid)
+    }
 }
