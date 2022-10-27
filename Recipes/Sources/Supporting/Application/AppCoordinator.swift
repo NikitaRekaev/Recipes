ca38816d20f817079ca18c98f34b79835da731f3
NikitaRekaev <nvvvvk@gmail.com>
2022-10-27 14:11:33 +0600

    Added AppCoordiator

---
+++ Recipes/Sources/Supporting/Application/AppCoordinator.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+final class AppCoordinator: Coordinator {
+    
+    // MARK: - Properties
+    
+    let window: UIWindow?
+    let rootNavigationController: UINavigationController = {
+        let navigationController = UINavigationController()
+        navigationController.navigationBar.barTintColor = R.color.backgroundColor()
+        navigationController.navigationBar.prefersLargeTitles = true
+        return navigationController
+    }()
+    
+    let networkTask: NetworkTask
+    let repository: Repository
+    
+    // MARK: - Coordinator
+    
+    init(window: UIWindow) {
+        self.window = window
+        networkTask = NetworkTask()
+        repository = Repository(networkTask: networkTask)
+    }
+    
+    override func start() {
+        guard let window = window else {
+            return
+        }
+        
+        window.rootViewController = rootNavigationController
+        window.makeKeyAndVisible()
+        
+        // MARK: - Show List
+        
+        let listCoordinator = ListCoordinator(rootNavigationController: rootNavigationController,
+                                              repository: repository)
+        
+        listCoordinator.delegate = self
+        addChildCoordinator(listCoordinator)
+        listCoordinator.start()
+    }
+    
+}
