17825a1701b7e3429d2c8408dbd159888928d5d1
NikitaRekaev <nvvvvk@gmail.com>
2022-10-27 14:13:00 +0600

    Added viewModel for ListViewController

--- Recipes/Sources/Modules/ListScreen/Controllers/ListViewController.swift
+++ Recipes/Sources/Modules/ListScreen/Controllers/ListViewController.swift
@@ -2,6 +2,17 @@ import UIKit
 
 class ListViewController: UIViewController {
 
+    let viewModel: ListViewModel
+    
+    init(viewModel: ListViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
