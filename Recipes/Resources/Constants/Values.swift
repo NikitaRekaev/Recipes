47d1ab4bdd793aea9ca570aed78d83bf3c7546f4
NikitaRekaev <nvvvvk@gmail.com>
2022-10-27 12:40:23 +0600

    Added Constants Values

---
+++ Recipes/Resources/Constants/Values.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+struct Constants {
+    
+    struct Design {
+        static let cornerRadiusError = CGFloat(22)
+        static let cornerRadiusMain = CGFloat(15)
+        static let spacingMain = CGFloat(22)
+        static let basicInset = CGFloat(20)
+        static let borderWidth = CGFloat(1)
+    }
+    
+    struct Inset {
+        static let huge = CGFloat(30)
+        static let classic = CGFloat(20)
+        static let small = CGFloat(18)
+        static let tiny = CGFloat(15)
+    }
+    
+    struct API {
+        static let baseURL = "https://test.kode-t.ru/"
+    }
+    
+    struct NavigationBarTitle {
+        static let recipes = "Recipes"
+        static let recipeDetails = "RecipeDetails"
+    }
+    
+    struct ErrorType {
+        static let basic = "Something went wrong"
+        static let noInternet = "No Internet"
+    }
+    
+    struct ButtonTitle {
+        static let refresh = "Refresh"
+    }
+    
+    struct ErrorText {
+        static let basic = "The problem is on our side, we are already looking into it. Please try refreshing the screen later."
+        static let noInternet = "Try refreshing the screen when communication is restored."
+
+    }
+    
+    struct Description {
+        static let empty = "No description provided."
+    }
+    
+    struct DateDummy {
+        static let recipeCell = "Last update: "
+        static let recipeDetails = "Last Recipe Update:\n"
+    }
+    
+}
