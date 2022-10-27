55ad0a608814b20628e40e6bf21d56272477316e
NikitaRekaev <nvvvvk@gmail.com>
2022-10-27 15:18:06 +0600

    Added ListView

--- Recipes/Sources/Modules/ListScreen/Views/ListView.swift
+++ Recipes/Sources/Modules/ListScreen/Views/ListView.swift
@@ -3,4 +3,31 @@ import UIKit
 
 final class ListView: BaseView {
     
+    // MARK: - UI Elements
+    
+    let tableView = UITableView()
+    let searchController = UISearchController()
+    
+    // MARK: - Configure
+    
+    override func configureAppearance() {
+        backgroundColor = .white
+    }
+    
+    override func configureUI() {
+        addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - Private Methods
+
+private extension ListView {
+    private func setupTableView() {
+        tableView.separatorStyle = .none
+        tableView.showsVerticalScrollIndicator = false
+        tableView.showsHorizontalScrollIndicator = false
+    }
 }
